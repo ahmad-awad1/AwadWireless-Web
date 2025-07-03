a37a0942dd9ea94084d79815fdbb8b94eb09c372
Ahmad Awad <aawad1@uncc.edu>
2025-07-03 13:28:58 -0400

    initail

---
+++ JS/HomepageJS.js
@@ -0,0 +1,73 @@
+//USE stri; 
+
+//document.addEventListener('DOMContentLoaded', function() {
+//   var dropdown = document.querySelector('.dropdown');
+//   dropdown.addEventListener('click', function() {
+//     dropdown.classList.toggle('active');
+//   });
+// });
+
+// // document.addEventListener('DOMContentLoaded', function() {
+// //   var dropdown = document.querySelector('.dropdown');
+// //   dropdown.addEventListener('click', function() {
+// //     dropdown.classList.toggle('active');
+// //   });
+// // });
+
+// function toggleDropdown(dropdownId) {
+//   var dropdown = document.getElementById(dropdownId);
+//   dropdown.classList.toggle('open');
+// }
+
+// // Register the function to be called when the DOM is fully loaded
+// document.addEventListener('DOMContentLoaded', function() {
+//   // Call the toggleDropdown function when the button is clicked for business hours
+//   document.getElementById('hoursDropdown').addEventListener('click', function() {
+//     toggleDropdown('hoursDropdown');
+//   });
+
+//   // Call the toggleDropdown function when the button is clicked for services
+//   document.getElementById('servicesDropdown').addEventListener('click', function() {
+//     toggleDropdown('servicesDropdown');
+//   });
+// });
+
+// function toggleDropdown(event) {
+//   var dropdown = event.target.parentNode;
+//   dropdown.classList.toggle('open');
+//   event.stopPropagation();
+// }
+
+// // Register the function to be called when the DOM is fully loaded
+// document.addEventListener('DOMContentLoaded', function() {
+//   // Close the dropdowns when clicking outside of them
+//   document.addEventListener('click', function() {
+//     var dropdowns = document.getElementsByClassName('dropdown');
+//     for (var i = 0; i < dropdowns.length; i++) {
+//       dropdowns[i].classList.remove('open');
+//     }
+//   });
+// });
+// function toggleDropdown(event) {
+//   var dropdown = event.target.parentNode;
+//   var dropdownIcon = dropdown.querySelector('#dropdownIcon');
+
+//   dropdown.classList.toggle('open');
+//   dropdownIcon.classList.toggle('rotate');
+//   event.stopPropagation();
+// }
+
+// // Register the function to be called when the DOM is fully loaded
+// document.addEventListener('DOMContentLoaded', function() {
+//   // Close the dropdowns when clicking outside of them
+//   document.addEventListener('click', function() {
+//     var dropdowns = document.getElementsByClassName('dropdown');
+//     var dropdownIcons = document.getElementsByClassName('fa-caret-down');
+
+//     for (var i = 0; i < dropdowns.length; i++) {
+//       dropdowns[i].classList.remove('open');
+//       dropdownIcons[i].classList.remove('rotate');
+//     }
+//   });
+// });
+
